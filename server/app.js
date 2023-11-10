a920118addae75debdae7a12fdd4230e07077763
web-island <island.web.studio@gmail.com>
2023-11-10 11:19:28 +0200

    fixed routing

--- server/app.js
+++ server/app.js
@@ -8,21 +8,26 @@ $('.home').load('./views/home.html');
 $('.profile').load('./views/profile.html');clearImmediate
 $('.my_playlists').load('./views/my_playlists.html');
 
+//Подключение модальных окон
+$('#login-modal').load('./views/modals/login.html');
+
 
 
 // аля react rout только проще и быстрее
 function showBlock(name) {
     
+    // Проверяем есть ли в sessionStorage запись о последнем нажатом меню и если есть то скрываем его блок
+    if(sessionStorage.getItem('menu-button-click')) { $(`.${sessionStorage.getItem('menu-button-click')}`).css('display', 'none') }
+
+    // Записываем в sessionStorage id нового нажатого меню
+    sessionStorage.setItem('menu-button-click', name);
+
+    // Получаем блок с id="main-window"
     const mainWindow = document.getElementById('main-window');
 
-    // Получаем все дочерние элементы div внутри "main-window"
-    const divs = mainWindow.querySelectorAll('div');
-
-    // Перебираем все дочерние элементы и присваиваем им стиль display: none
-    divs.forEach(div => { div.style.display = 'none' });
-
-    //присваиваем display: block для полученого блока
+    // Присваиваем блоку стиль display: block
     $(`.${name}`).css('display', 'block');
+    
 }
 
 SOCK_.on("connect", () => { console.log("Connect to server!") });
